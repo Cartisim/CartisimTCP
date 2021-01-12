96c25b641fa1f9cb80c0c7678a13b3e01a6bcacc
Cole Moore <Cartisim@icloud.com>
2021-01-12 09:11:42 -0800

    fixed production address again

--- Sources/CartisimTCP/main.swift
+++ Sources/CartisimTCP/main.swift
@@ -7,7 +7,7 @@
 #if DEBUG || LOCAL
 let server = TCPServer(host: "localhost", port: 8081)
 #else
-let server = TCPServer(host: "tcp.cartisin.io", port: 8081)
+let server = TCPServer(host: "tcp.cartisim.io", port: 8081)
 #endif
 do {
     print("Server is running")
