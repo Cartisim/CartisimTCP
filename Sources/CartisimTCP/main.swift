73f55047af8b4199f4f4b0a32a0e4dc5290ccc4c
Cole Moore <Cartisim@icloud.com>
2021-01-13 12:55:21 -0800

    add production host

--- Sources/CartisimTCP/main.swift
+++ Sources/CartisimTCP/main.swift
@@ -3,7 +3,12 @@
 //#endif
 
 
+
+#if DEBUG || LOCAL
 let server = TCPServer(host: "localhost", port: 8081)
+#else
+let server = TCPServer(host: "tcp.cartisim.io", port: 8081)
+#endif
 do {
     print("Server is running")
     try server.run()
