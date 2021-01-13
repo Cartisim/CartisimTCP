07a7e4aeedcd2647c34b8f0fca3b68d8b584dd83
Cole Moore <Cartisim@icloud.com>
2021-01-13 12:26:32 -0800

    specified host

--- Sources/CartisimTCP/main.swift
+++ Sources/CartisimTCP/main.swift
@@ -3,12 +3,7 @@
 //#endif
 
 
-
-#if DEBUG || LOCAL
 let server = TCPServer(host: "localhost", port: 8081)
-#else
-let server = TCPServer(host: "159.65.103.99", port: 8081)
-#endif
 do {
     print("Server is running")
     try server.run()
