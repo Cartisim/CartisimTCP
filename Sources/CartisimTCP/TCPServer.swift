f92248b1c7eea74368d7b2c4e4f0da7a807120f9
Cole Moore <Cartisim@icloud.com>
2021-01-15 16:41:47 -0800

    fixed bug where the handler  was sharing the TLS state across multiple connections

--- Sources/CartisimTCP/TCPServer.swift
+++ Sources/CartisimTCP/TCPServer.swift
@@ -16,41 +16,6 @@ public class TCPServer {
     
     let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
     
-//    private var serverBootstrap: ServerBootstrap {
-//        let basePath = FileManager().currentDirectoryPath
-//        let certPath = basePath + "/cert.pem"
-//        let keyPath = basePath + "/privkey.pem"
-//        let bootstrap = ServerBootstrap(group: group)
-//        do {
-//            let certs = try NIOSSLCertificate.fromPEMFile(certPath)
-//                .map { NIOSSLCertificateSource.certificate($0) }
-//            let privateKey = try NIOSSLPrivateKey(file: keyPath, format: .pem)
-//            let configuration = TLSConfiguration.forServer(certificateChain: certs,
-//                                                           privateKey: .privateKey( privateKey))
-//            let sslContext = try NIOSSLContext(configuration: configuration)
-//            let handler = NIOSSLServerHandler(context: sslContext)
-//            return bootstrap
-//                .serverChannelOption(ChannelOptions.backlog, value: 256)
-//                .serverChannelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
-//                .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-//                .childChannelInitializer { channel in
-//                    channel.pipeline.addHandler(handler).flatMap { v in
-//                        channel.pipeline.addHandler(BackPressureHandler()).flatMap { v in
-//                            channel.pipeline.addHandler(ChatHandler())
-//                        }
-//                    }
-//                }
-//                .childChannelOption(ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY), value: 1)
-//                .childChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-//                .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 16)
-//                .childChannelOption(ChannelOptions.recvAllocator, value: AdaptiveRecvByteBufferAllocator())
-//        } catch {
-//            print(error, "OUR ERROR")
-//        }
-//        return bootstrap
-//    }
-    
-    
     private var serverBootstrap: ServerBootstrap {
         #if DEBUG || LOCAL
         return ServerBootstrap(group: group)
@@ -80,26 +45,19 @@ public class TCPServer {
         
         
         let sslContext = try? NIOSSLContext(configuration: tls)
-        let handler = NIOSSLServerHandler(context: sslContext!)
+        
         
         return ServerBootstrap(group: group)
-            .serverChannelOption(ChannelOptions.backlog, value: 256)
-            .serverChannelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
-            .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-            
             
             .childChannelInitializer { channel in
-                channel.pipeline.addHandler(handler).flatMap { v in
+                channel.pipeline.addHandler(NIOSSLServerHandler(context: sslContext!)).flatMap { v in
                     channel.pipeline.addHandler(BackPressureHandler()).flatMap { v in
                         channel.pipeline.addHandler(ChatHandler())
                     }
                 }
             }
-            
-            .childChannelOption(ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY), value: 1)
             .childChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
-            .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 16)
-            .childChannelOption(ChannelOptions.recvAllocator, value: AdaptiveRecvByteBufferAllocator())
+        
         #endif
     }
     
@@ -113,58 +71,6 @@ public class TCPServer {
         }
         print("closed server")
     }
-    
-//    func run() throws {
-//        guard let host = host else {
-//            throw TCPError.invalidHost
-//        }
-//        guard let port = port else {
-//            throw TCPError.invalidPort
-//        }
-//        // First argument is the program path
-//        let arguments = CommandLine.arguments
-//        let arg1 = arguments.dropFirst().first
-//        let arg2 = arguments.dropFirst(2).first
-//        enum BindTo {
-//            case ip(host: String, port: Int)
-//            case unixDomainSocket(path: String)
-//        }
-//
-//        let bindTarget: BindTo
-//
-//        switch (arg1, arg1.flatMap(Int.init), arg2.flatMap(Int.init)) {
-//        case (.some(let h), _ , .some(let p)):
-//            /* we got two arguments, let's interpret that as host and port */
-//            bindTarget = .ip(host: h, port: p)
-//
-//        case (let portString?, .none, _):
-//            // Couldn't parse as number, expecting unix domain socket path.
-//            bindTarget = .unixDomainSocket(path: portString)
-//
-//        case (_, let p?, _):
-//            // Only one argument --> port.
-//            bindTarget = .ip(host: host, port: p)
-//
-//        default:
-//            bindTarget = .ip(host: host, port: port)
-//        }
-//        let channel = try { () -> Channel in
-//            switch bindTarget {
-//            case .ip(let host, let port):
-//                return try serverBootstrap.bind(host: host, port: port).wait()
-//            case .unixDomainSocket(let path):
-//                return try serverBootstrap.bind(unixDomainSocketPath: path).wait()
-//            }
-//        }()
-//        guard let localAddress = channel.localAddress else {
-//            fatalError("Address was unable to bind. Please check that the socket was not closed or that the address family was understood.")
-//        }
-//        print("Server started and listening on \(localAddress)")
-//        //  This will never unblock as we don't close the ServerChannel.
-////        try? fetchKeys()
-//        try channel.closeFuture.wait()
-//
-//    }
     
     func run() throws {
         guard let host = host else {
@@ -232,47 +138,47 @@ fileprivate func fetchKeys() throws {
     let certs = try NIOSSLCertificate.fromPEMFile(certPath)
         .map { NIOSSLCertificateSource.certificate($0) }
     do {
-    let privateKey = try NIOSSLPrivateKey(file: keyPath, format: .pem)
-    let configuration = TLSConfiguration.forClient(minimumTLSVersion: .tlsv12, certificateChain: certs,
-                                                                                privateKey: .privateKey( privateKey))
-  
-    let httpClient = HTTPClient(eventLoopGroupProvider: .createNew, configuration: HTTPClient.Configuration(tlsConfiguration: configuration))
-    var request = try HTTPClient.Request(url: "\(Constants.BASE_URL)fetchKeys", method: .GET)
-    request.headers.add(name: "User-Agent", value: "Swift HTTPClient")
-    request.headers.add(name: "Content-Type", value: "application/json")
-    request.headers.add(name: "Authorization", value: "Bearer")
-    request.headers.add(name: "Connection", value: "keep-alive")
-    request.headers.add(name: "Content-Length", value: "")
-    request.headers.add(name: "Date", value: "\(Date())")
-    request.headers.add(name: "Server", value: "TCPCartisim")
-    request.headers.add(name: "content-security-policy", value: "default-src 'none'")
-    request.headers.add(name: "x-content-type-options", value: "nosniff")
-    request.headers.add(name: "x-frame-options", value: "DENY")
-    request.headers.add(name: "x-xss-protection", value: "1; mode=block")
-    httpClient.execute(request: request)
-        .whenComplete { result in
-            switch result {
-            case .failure(let error):
-                print(error, "FAILURE ERROR")
-            case .success(let response):
-                if response.status == .ok {
-                    do {
-                        guard let responseData = response.body else {return}
-                        let objects = try JSONDecoder().decode([Keys].self, from: responseData)
-                        KeyData.shared.keychainEncryptionKey = objects.last?.keychainEncryptionKey ?? ""
-                    } catch {
-                        print(error, "ERROR")
+        let privateKey = try NIOSSLPrivateKey(file: keyPath, format: .pem)
+        let configuration = TLSConfiguration.forClient(minimumTLSVersion: .tlsv12, certificateChain: certs,
+                                                       privateKey: .privateKey( privateKey))
+        
+        let httpClient = HTTPClient(eventLoopGroupProvider: .createNew, configuration: HTTPClient.Configuration(tlsConfiguration: configuration))
+        var request = try HTTPClient.Request(url: "\(Constants.BASE_URL)fetchKeys", method: .GET)
+        request.headers.add(name: "User-Agent", value: "Swift HTTPClient")
+        request.headers.add(name: "Content-Type", value: "application/json")
+        request.headers.add(name: "Authorization", value: "Bearer")
+        request.headers.add(name: "Connection", value: "keep-alive")
+        request.headers.add(name: "Content-Length", value: "")
+        request.headers.add(name: "Date", value: "\(Date())")
+        request.headers.add(name: "Server", value: "TCPCartisim")
+        request.headers.add(name: "content-security-policy", value: "default-src 'none'")
+        request.headers.add(name: "x-content-type-options", value: "nosniff")
+        request.headers.add(name: "x-frame-options", value: "DENY")
+        request.headers.add(name: "x-xss-protection", value: "1; mode=block")
+        httpClient.execute(request: request)
+            .whenComplete { result in
+                switch result {
+                case .failure(let error):
+                    print(error, "FAILURE ERROR")
+                case .success(let response):
+                    if response.status == .ok {
+                        do {
+                            guard let responseData = response.body else {return}
+                            let objects = try JSONDecoder().decode([Keys].self, from: responseData)
+                            KeyData.shared.keychainEncryptionKey = objects.last?.keychainEncryptionKey ?? ""
+                        } catch {
+                            print(error, "ERROR")
+                        }
+                    } else {
+                        print(response.status, "Remote Error")
                     }
-                } else {
-                    print(response.status, "Remote Error")
+                }
+                do {
+                    try httpClient.syncShutdown()
+                } catch {
+                    print(error, "Error")
                 }
             }
-            do {
-                try httpClient.syncShutdown()
-            } catch {
-                print(error, "Error")
-            }
-        }
     } catch {
         print(error)
     }
@@ -304,3 +210,13 @@ struct KeyData {
 enum TCPErrors: Error {
     case sslContextError(String)
 }
+
+
+//            .serverChannelOption(ChannelOptions.backlog, value: 256)
+//            .serverChannelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
+//            .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+//
+
+//            .childChannelOption(ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY), value: 1)
+//            .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 16)
+//            .childChannelOption(ChannelOptions.recvAllocator, value: AdaptiveRecvByteBufferAllocator())
