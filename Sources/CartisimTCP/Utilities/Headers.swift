291ac92979aa0d3c88a3cb660f51124a75871ae9
Cole Moore <Cartisim@icloud.com>
2021-02-27 20:47:45 -0800

    Adjusted Headers

--- Sources/CartisimTCP/Utilities/Headers.swift
+++ Sources/CartisimTCP/Utilities/Headers.swift
@@ -3,6 +3,7 @@ import Foundation
 
 final class Headers {
     static func headers(token: String) -> HTTPHeaders {
-        return HTTPHeaders([("User-Agent", "Swift HTTPClient"), ("Content-Type", "application/json"), ("Authorization", "Bearer: \(token)"), ("Connection", "keep-alive"), ("Date", "\(Date())"), ("Server", "TCPCartisim"), ("content-security-policy", "default-src 'none'"), ("x-content-type-options", "nosniff"), ("x-frame-options", "DENY"), ("x-xss-protection", "1; mode=block")])
+//        return HTTPHeaders([("User-Agent", "Swift HTTPClient"), ("Content-Type", "application/json"), ("Authorization", "Bearer: \(token)"), ("Connection", "keep-alive"), ("Date", "\(Date())"), ("Server", "TCPCartisim"), ("content-security-policy", "default-src 'none'"), ("x-content-type-options", "nosniff"), ("x-frame-options", "DENY"), ("x-xss-protection", "1; mode=block")])
+        return HTTPHeaders([("User-Agent", "Swift HTTPClient"), ("Content-Type", "application/json"), ("Authorization", "Bearer: \(token)"), ("Connection", "keep-alive"), ("Date", "\(Date())"), ("Server", "TCPCartisim")])
     }
 }
